6105b2b467051abc61cf81192ac11769b5d7c41b
JNing73 <94809226+JNing73@users.noreply.github.com>
2025-01-28 15:46:18 +1300

    Show saved inputs as an unordered list

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const saveBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
 
 const myLeads = [];
 
@@ -8,4 +9,12 @@ saveBtn.addEventListener("click", saveLead);
 function saveLead() {
     myLeads.push(inputEl.value);
     console.log(myLeads);
+    renderList();
+}
+
+function renderList() {
+    lastEntry = myLeads.length - 1;
+    let liEl = document.createElement("li");
+    liEl.textContent = myLeads[lastEntry]; //<li>value</li>
+    ulEl.appendChild(liEl); 
 }
